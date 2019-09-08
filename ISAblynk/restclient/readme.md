f0391078a3550929a0b50b470d0954ba6609feca
sampad <smpdmohanty@gmail.com>
2019-09-07 18:17:53 -0700

    auto commit by sampad

---
+++ ISAblynk/restclient/readme.md
@@ -0,0 +1,3 @@
+Run python ISAblynk.py
+See if there is any redirect to a new IP and Port.
+If there is, use that IP and Port in the file restcmd.rb
--- ISAblynk/restclient/restcmd.rb
+++ ISAblynk/restclient/restcmd.rb
@@ -3,27 +3,30 @@ require 'rest-client'
 
 # get cmdline args
 cmd,pin,value=ARGV
-auth_token=File.read("authtoken.txt").chop
-
+auth_token=File.read("../token.txt").chop
+puts "TOKEN: #{auth_token}"
 
 urlBase="http://blynk-cloud.com/#{auth_token}"
+urlBase="http://188.166.206.43/#{auth_token}"
 
 
 case cmd
 when "help"
-  print "ARGS -> status | update (pin) (value)"
+  print "ARGS -> status | update (pin) (value)\n"
   exit
 when "update"
   url=urlBase+"/update/#{pin}?value=#{value}"
 when "status"
   url=urlBase+"/isHardwareConnected"
 else
-  print "ARGS -> status | update (pin) (value)"
+  print "ARGS -> status | update (pin) (value)\n"
   exit  
 end 
 
 puts "\nENDPOINT->\n#{url}\n"
 result=RestClient.get(url)
+
+puts result
 
 puts "\nHEADERS->"
 result.headers.each { |header| print "#{header}\n" }
